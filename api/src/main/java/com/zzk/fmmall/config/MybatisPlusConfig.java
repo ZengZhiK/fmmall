c814fdae41c6cc23dfbed29b06723cc59533050d
ZengZhiK <BUCTZengZK@163.com>
2022-01-14 11:14:41 +0800

    fix:修复逻辑删除bug

---
+++ api/src/main/java/com/zzk/fmmall/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.zzk.fmmall.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zzk
+ * @create 2022-01-14 11:11
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+}
